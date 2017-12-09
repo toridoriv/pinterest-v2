4a85e5f855a7fe0626ff755436d7353eba7c6e63
malditoprotozoo <virodriguez.fe@gmail.com>
2017-12-09 00:08:38 -0300

    First Commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#tags").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true
+  })
+});
